e59e700b83f4f0c0628fde5b4d875080786bfb6f
Syedalthafusain <svsyedms@gmail.com>
2025-11-11 11:23:58 +0530

    tiles measurement

---
+++ main.js
@@ -0,0 +1,6 @@
+function calculation(params) {
+    const length=document.getElementById("length").value
+    const height=document.getElementById("height").value
+    const result=document.getElementById("result")
+    
+}
