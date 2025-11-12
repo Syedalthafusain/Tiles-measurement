96f1000c63ae58a49d944954a72ae231c0c3cfb4
Syedalthafusain <svsyedms@gmail.com>
2025-11-12 12:02:58 +0530

    18x12 box calculation done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function calculation(params) {
-    const length=document.getElementById("length").value
-    const height=document.getElementById("height").value
-    const result=document.getElementById("result")
-    
+document.getElementById("calculatebtn").addEventListener("click", calculatebtn);
+function calculatebtn() {
+  const length = Number(document.getElementById("length").value);
+  const height = Number(document.getElementById("height").value);
+  const result = document.getElementById("result");
+  const boxcount = (length * height) / 9;
+  result.innerHTML = boxcount.toFixed(3) +" boxes";
 }
